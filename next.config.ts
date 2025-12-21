39acc40ba7269f8616c8de32af76b20439834634
Slava <slavachuval@gmail.com>
2025-12-21 12:35:52 +1030

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
     env: {
-        NEXT_PUBLIC_SUPABASE_URL: "https://scyrvikyachtrlkobphj.supabase.co",
-        NEXT_PUBLIC_SUPABASE_PROJECT_ID: "scyrvikyachtrlkobphj",
-        NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY: "sb_publishable_gSgGiWvnMiKrLyWwf-cLTA_BSAN_4KK",
+        NEXT_PUBLIC_SUPABASE_URL: "",
+        NEXT_PUBLIC_SUPABASE_PROJECT_ID: "",
+        NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY: "",
         // TODO: fix when we merge this branch to master/production
-        S3_PATH: process.env.NODE_ENV == 'development' ? 'http://jp-website-frontend-staging.s3-website-ap-southeast-2.amazonaws.com'
-            : 'http://jp-website-frontend-staging.s3-website-ap-southeast-2.amazonaws.com',
+        S3_PATH: "",
     }
 };
 
